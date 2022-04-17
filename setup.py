dc45a091858605b2419ed49189dc7f9b5c157853
DerPercy <christian.chsc.schmidt@gmail.com>
2022-04-17 12:28:23 +0200

    update setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='myutils',
+   version='0.0.1-alpha',
+   description='My utils module',
+   author='DerPercy',
+   author_email='',
+   packages=['myutils'],  #same as name
+   install_requires=[], #external packages as dependencies
+)
