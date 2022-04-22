ad25684dcafe37ab7eb0ca88b2dd70f7777ac915
DerPercy <christian.chsc.schmidt@gmail.com>
2022-04-22 13:11:49 +0200

    Update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='myutils',
-   version='0.0.1-alpha',
+   version='0.0.2-alpha',
    description='My utils module',
    author='DerPercy',
    author_email='',
