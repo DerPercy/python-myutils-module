ac039af5d6589d31810f4e0c7235da86ceac6eea
DerPercy <christian.chsc.schmidt@gmail.com>
2022-04-22 13:19:03 +0200

    Update

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
    name='myutils',
-   version='0.0.3-alpha',
+   version='0.0.4-alpha',
    description='My utils module',
    author='DerPercy',
    author_email='',
    packages=['myutils'],  #same as name
    include_dirs=['storage'],
-   install_requires=['openpyxl'], #external packages as dependencies
+   install_requires=['openpyxl','.storage'], #external packages as dependencies
 )
