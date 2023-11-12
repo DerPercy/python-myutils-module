9a27ce00adb08ad7ebff02bac54cb21e6cf6a84b
DerPercy <christian.chsc.schmidt@gmail.com>
2023-11-12 08:32:51 +0100

    tests

--- tests/test_xlsx.py
+++ tests/test_xlsx.py
@@ -59,6 +59,45 @@ def test_xlsx_storage_stopat(caplog):
     assert isinstance(entities, list)
     assert len(entities) == 3
 
+def test_create_entry(caplog):
+    """
+    Test the creation of an entry
+    """
+    filepattern = os.path.join(os.path.dirname(__file__), "data/xlsx/create/appointments_{year}.xlsx")
+    settings = {
+        "xlsx": {
+            "filepattern": filepattern,
+            "filecontent": {
+                "stopatnonevalue": "0",
+                "startrow": 1,
+                "columns": {
+                    "0": "date",
+                    }
+            },
+            "properties": {
+                "year": { "pattern": "\\d{4}" }
+            }
+        }
+    }
+    storage = XLSXStorage(settings)
+    
+    entity = storage.create({
+        "year": "2023",
+        "date": "01.01.2023"
+    })
+    
+    """ File needs to be created """
+    checkFile = os.path.join(os.path.dirname(__file__), "data/xlsx/create/appointments_2023.xlsx")
+    assert os.path.exists(checkFile) == False
+
+    """ query should have 1 line"""
+    entities = storage.query({})
+    assert isinstance(entities, list)
+    assert len(entities) == 1
+
+    # cleanUp file
+    os.remove(checkFile)
+
 
 def test_extract_values(caplog):
     obj = {}
